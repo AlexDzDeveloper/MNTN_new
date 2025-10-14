b4c58a40955b052d09a11aaa73b3abd184d15936
AlexDz <alexdzdevelop@gmail.com>
2025-10-14 23:43:10 +0300

    Work with animation

--- js/blocks/_hiker-assessment.js
+++ js/blocks/_hiker-assessment.js
@@ -12,47 +12,13 @@ document.addEventListener('DOMContentLoaded', () => {
 		const parent = e.target.closest('.article');
 
 		console.log(parent);
-		parent.style.display = 'none';
-		parent.style.transition = 'display 0.5s ease';//не працює для display, переробити на visability
-		hiker.style.display = "flex";
-
+		parent.classList.add('hidden');
+		hiker.classList.remove('hidden');
 	});
-	// console.log(articleBtn);
-	// articleBtn.addEventListener('click', () => {
-	// 	article.style.display = "none"
-	// });
-
-
-	/*
-					.article {
-					opacity: 1;
-					visibility: visible;
-					transition: opacity 0.5s ease, visibility 0.5s ease;
-					}
-
-					.article.hidden {
-					opacity: 0;
-					visibility: hidden;
-					}
-
-
-
-					articleBtn.addEventListener('click', (e) => {
-					const parent = e.target.closest('.article');
-					parent.classList.add('hidden');
-
-					setTimeout(() => {
-						hiker.style.display = "flex";
-					}, 500); // після завершення анімації
-					});
-
-	*/
-
-
 
 	hikerBtn.addEventListener('click', () => {
-		hiker.style.display = 'none';
-		article.closest('.article').style.display = 'flex';
+		article.closest('.article').classList.remove('hidden');
+		hiker.classList.add('hidden');
 		console.log(article);
 	})
 });
